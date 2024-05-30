0ae83dd328fb7952109ec6aa8753cf42ff818858
Hariki <nxh0809@gmail.com>
2024-05-30 09:24:32 +0700

    Update ObsBuilder to only reflect changes on new value

--- lib/base/rx/obs_builder.dart
+++ lib/base/rx/obs_builder.dart
@@ -7,9 +7,14 @@ class ObsBuilder extends StatefulWidget {
   final List<Stream> streams;
   final Widget Function(BuildContext context) builder;
 
+  /// By default, the widget will rebuild whenever the stream emits a new value.
+  /// If you want to rebuild the widget even if the new value is the same as the old one, set this to true.
+  final bool rebuildOnSameValueChange;
+
   const ObsBuilder({
     this.streams = const [],
     required this.builder,
+    this.rebuildOnSameValueChange = false,
   });
 
   @override
@@ -22,8 +27,13 @@ class _ObsBuilderState extends State<ObsBuilder> {
   @override
   void initState() {
     super.initState();
+    var stream = Rx.merge(widget.streams);
+
+    if (!widget.rebuildOnSameValueChange) {
+      stream = stream.distinct();
+    }
     // Rebuild the widget whenever the stream changes
-    subscription = Rx.merge(widget.streams).listen((_) => setState(() {}));
+    subscription = stream.listen((_) => setState(() {}));
   }
 
   @override
